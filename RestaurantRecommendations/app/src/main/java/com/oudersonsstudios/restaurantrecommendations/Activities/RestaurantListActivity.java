a229ae34ba041b78744a0c15b7763ec5d68549ac
FuzzyMoney <polegomaniac@yahoo.com>
2017-03-06 23:11:45 -0600

    More stuff
    Clickable list things, starting anyways

--- RestaurantRecommendations/app/src/main/java/com/oudersonsstudios/restaurantrecommendations/Activities/RestaurantListActivity.java
+++ RestaurantRecommendations/app/src/main/java/com/oudersonsstudios/restaurantrecommendations/Activities/RestaurantListActivity.java
@@ -1,11 +1,14 @@
 package com.oudersonsstudios.restaurantrecommendations.Activities;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.oudersonsstudios.restaurantrecommendations.R;
 
@@ -18,6 +21,15 @@ public class RestaurantListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ListView restaurantList = (ListView) findViewById(R.id.restaurantListView);
+
+        restaurantList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                //Whatever we do when we click on the list goes here.
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +39,4 @@ public class RestaurantListActivity extends AppCompatActivity {
             }
         });
     }
-
 }
